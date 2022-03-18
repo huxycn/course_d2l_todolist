b55627454f60b21d3105cdb63a876701d9c1f6c4
huxiaoyang <545960442@qq.com>
2022-03-18 14:20:18 +0800

    commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import json
+from bs4 import BeautifulSoup
+
+
+with open('html_source/source.html', 'r') as f:
+    soup = BeautifulSoup(f.read(), 'lxml')
+
+h2_list = soup.findAll('h2', attrs={'class': 'fs-4'})
+div_list = soup.findAll('div', attrs={'class': 'module'})
+
+course = {
+    'title': '动手学深度学习',
+    'chapters': []
+}
+
+for h2, div in zip(h2_list, div_list):
+    chapter = {
+        'title': h2['id'],
+        'lectures': []
+    }
+    for dl in div.find('dl').findAll('dl'):
+        lecture = {
+            'title': dl.text.strip(),
+            'assets': {
+                'doc': '',
+                'pdf': '',
+                'notebook': '',
+                'video': ''
+            }
+        }
+        for a in dl.findAll('a'):
+            url = a['href']
+            url = 'https://courses.d2l.ai/zh-v2/' + url if url.startswith('assets') else url
+            if 'zh-v2' in url:
+                lecture['assets']['doc'] = url
+            if 'pdfs' in url:
+                lecture['assets']['pdf'] = url
+            if 'notebooks' in url:
+                lecture['assets']['notebook'] = url
+            if 'video' in url:
+                lecture['assets']['video'] = url
+
+        chapter['lectures'].append(lecture)
+    course['chapters'].append(chapter)
+
+with open('json/course.json', 'w') as f:
+    f.write(json.dumps(course, ensure_ascii=False))
+
+with open('json/course.json', 'r') as f:
+    course = json.load(f)
+for i, chapter in enumerate(course['chapters']):
+    with open(f"md/chapter_{i}.md", 'w') as f:
+        f.write(f"## {chapter['title']}\n")
+        f.write('| Title | Doc | PDF | Notebook | Video | 打卡 |\n')
+        f.write('| --- | :---: | :---: | :---: | :---: | :---: |\n')
+
+        for lecture in chapter['lectures']:
+            line = f"| {lecture['title']} | "
+            for k, v in lecture['assets'].items():
+                if v:
+                    line += f"[{k}]({v}) |"
+                else:
+                    line += "- |"
+            line += '\n'
+            f.write(line)
+
